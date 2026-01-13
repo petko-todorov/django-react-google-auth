8442bc6658ec1aa1e4802e8a4452c788d1d120a8
Petko Todorov <p.todorov1015@gmail.com>
2026-01-13 14:04:15 +0200

    Integrate Google OAuth and AuthProvider to App.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { AuthProvider } from './AuthContext';
+import Home from './components/Home';
+
 function App() {
-    return <></>;
+    const GOOGLE_CLIENT_ID =
+        '984896660136-9qah6rb65u01dagd80b86jp94g7f9gbb.apps.googleusercontent.com';
+
+    return (
+        <>
+            <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+                <AuthProvider>
+                    <Home />
+                </AuthProvider>
+            </GoogleOAuthProvider>
+        </>
+    );
 }
 
 export default App;
