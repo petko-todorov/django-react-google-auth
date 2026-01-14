26994eb9d3cb9230032fd69fb001532c200b52a6
Petko Todorov <p.todorov1015@gmail.com>
2026-01-14 11:26:27 +0200

    Update Google login view in URL patterns
    Replaces GoogleLoginView with GoogleAuthCodeLoginView in the 'google-popup-login' URL pattern to reflect changes in the authentication flow.

--- backend/client_side_flow/urls.py
+++ backend/client_side_flow/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from client_side_flow.views import GoogleLoginView, VerifySessionView, LogoutView
+from client_side_flow.views import VerifySessionView, GoogleAuthCodeLoginView, LogoutView
 
 urlpatterns = [
-    path('google-popup-login/', GoogleLoginView.as_view(), name='google_login'),
+    path('google-popup-login/', GoogleAuthCodeLoginView.as_view(), name='google_login'),
     path('verify/', VerifySessionView.as_view(), name='verify_session'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
