b93e41611b3e19eef37ee756aed77fd62f6d17f8
Petko Todorov <p.todorov1015@gmail.com>
2026-01-16 13:28:30 +0200

    Implement routing for Google OAuth flow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,17 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from './AuthContext';
 import Home from './components/Home';
+import { Routes, Route, Navigate } from 'react-router';
+import GoogleCallback from './components/GoogleCallback';
 
 function App() {
-    const GOOGLE_CLIENT_ID =
-        '984896660136-9qah6rb65u01dagd80b86jp94g7f9gbb.apps.googleusercontent.com';
-
     return (
         <>
-            <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-                <AuthProvider>
-                    <Home />
-                </AuthProvider>
-            </GoogleOAuthProvider>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/google-callback" element={<GoogleCallback />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
         </>
     );
 }
