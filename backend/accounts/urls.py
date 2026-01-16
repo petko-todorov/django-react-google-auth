1ff9737eed6a04b07b4c86bf8cb7a3b187d657ff
Petko Todorov <p.todorov1015@gmail.com>
2026-01-16 13:31:14 +0200

    Update Google login URL name and path

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from accounts.views import VerifySessionView, GooglePopupLoginView, LogoutView
+from accounts.views import VerifySessionView, LogoutView, GoogleLoginView
 
 urlpatterns = [
     path('verify/', VerifySessionView.as_view(), name='verify_session'),
-    path('google-popup-login/', GooglePopupLoginView.as_view(), name='google_popup_login'),
+    path('google-login/', GoogleLoginView.as_view(), name='google_login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
