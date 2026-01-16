806dc59a6f4884a91be0c529717e382e7205f2b8
Petko Todorov <p.todorov1015@gmail.com>
2026-01-16 13:28:58 +0200

    Add routing and Google OAuth to app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { AuthProvider } from './AuthContext.jsx';
+
+const GOOGLE_CLIENT_ID =
+    '984896660136-9qah6rb65u01dagd80b86jp94g7f9gbb.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+            <AuthProvider>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </AuthProvider>
+        </GoogleOAuthProvider>
     </StrictMode>
 );
